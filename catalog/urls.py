c37be40d10291a1bf7e3afe0a8ecc15bc3eccdcd
David Kofman <dealvik@gmail.com>
2019-07-07 18:51:09 -0400

    Testing

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books123/', views.BookListView.as_view(), name='books123'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
@@ -12,18 +13,15 @@ urlpatterns = [
          views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
-
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
 ]
 
-
 # Add URLConf for librarian to renew a book.
 urlpatterns += [
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
-
 
 # Add URLConf to create, update, and delete authors
 urlpatterns += [
